f8ef4c7f037239d72d304d1494b6840e94008a0a
lupinge <849836178@qq.com>
2016-11-09 14:18:37 +0800

    app

---
+++ app/main.js
@@ -0,0 +1,5 @@
+require('./index.scss');
+var mod = require('./module1');
+
+mod.sayHi('jiao');
+mod.sayBye('du');
---
+++ app/module1.js
@@ -0,0 +1,7 @@
+console.log('Hello Webpack!');
+exports.sayHi = function (name) {
+    window.console.log('hi~'+name);
+};
+exports.sayBye = function (name) {
+    window.console.log('Bye~'+name);
+};
